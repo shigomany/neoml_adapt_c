495c410616daf904199658f47a7008a9fb8e72e2
Shigomany <ltropin@mail.ru>
2022-05-29 18:29:02 +0500

    Fix export Random

--- NeoML/include/NeoML/Random.h
+++ NeoML/include/NeoML/Random.h
@@ -17,63 +17,84 @@ limitations under the License.
 
 #include <NeoML/NeoMLDefs.h>
 
-namespace NeoML {
-
-// The class to generate random values
-// It uses the complementary-multiply-with-carry algorithm
-// C lag-1024, multiplier(a) = 108798, initial carry(c) = 12345678
-class NEOML_API CRandom {
-public:
-	explicit CRandom( unsigned int seed = 0xBADF00D );
-
-	// Resets to the starting state
-	void Reset( unsigned int seed );
-
-	// Returns the next random value
-	unsigned int Next();
-	// Returns a double value from a uniform distribution in [ min, max ) range
-	// If min == max, min is returned
-	double Uniform( double min, double max );
-	// Returns an int value from a uniform distribution in [ min, max ] range. Note that max return value is possible!
-	// If min == max, min is returned
-	int UniformInt( int min, int max );
-	// Returns a double value from a normal distribution N(mean, sigma)
-	double Normal( double mean, double sigma );
-
-	friend CArchive& operator<<( CArchive& archive, const CRandom& random);
-	friend CArchive& operator>>( CArchive& archive, CRandom& random);
-
-private:
-	static const int lagSize = 1024;
-	static const unsigned int stdLag[]; // the standard (pre-generated) lag
-	static const unsigned int multiplier = 108798;
-	static const unsigned int initialCarry = 12345678;
-
-	unsigned int lag[lagSize];
-	unsigned int carry;
-	int lagPosition;
-};
-
-inline CArchive& operator<<( CArchive& archive, const CRandom& random )
+namespace NeoML
 {
-	for(int i = 0; i < CRandom::lagSize; ++i) {
-		archive << (int)random.lag[i];
+
+	// The class to generate random values
+	// It uses the complementary-multiply-with-carry algorithm
+	// C lag-1024, multiplier(a) = 108798, initial carry(c) = 12345678
+	class NEOML_API CRandom
+	{
+	public:
+		explicit CRandom(unsigned int seed = 0xBADF00D);
+
+		// Resets to the starting state
+		void Reset(unsigned int seed);
+
+		// Returns the next random value
+		unsigned int Next();
+		// Returns a double value from a uniform distribution in [ min, max ) range
+		// If min == max, min is returned
+		double Uniform(double min, double max);
+		// Returns an int value from a uniform distribution in [ min, max ] range. Note that max return value is possible!
+		// If min == max, min is returned
+		int UniformInt(int min, int max);
+		// Returns a double value from a normal distribution N(mean, sigma)
+		double Normal(double mean, double sigma);
+
+		friend CArchive &operator<<(CArchive &archive, const CRandom &random);
+		friend CArchive &operator>>(CArchive &archive, CRandom &random);
+
+	private:
+		static const int lagSize = 1024;
+		static const unsigned int stdLag[]; // the standard (pre-generated) lag
+		static const unsigned int multiplier = 108798;
+		static const unsigned int initialCarry = 12345678;
+
+		unsigned int lag[lagSize];
+		unsigned int carry;
+		int lagPosition;
+	};
+
+	inline CArchive &operator<<(CArchive &archive, const CRandom &random)
+	{
+		for (int i = 0; i < CRandom::lagSize; ++i)
+		{
+			archive << (int)random.lag[i];
+		}
+		archive << (int)random.carry;
+		archive << random.lagPosition;
+
+		return archive;
 	}
-	archive << (int)random.carry;
-	archive << random.lagPosition;
 
-	return archive;
-}
+	inline CArchive &operator>>(CArchive &archive, CRandom &random)
+	{
+		for (int i = 0; i < CRandom::lagSize; ++i)
+		{
+			archive >> (int &)random.lag[i];
+		}
+		archive >> (int &)random.carry;
+		archive >> random.lagPosition;
 
-inline CArchive& operator>>( CArchive& archive, CRandom& random )
-{
-	for(int i = 0; i < CRandom::lagSize; ++i) {
-		archive >> (int&)random.lag[i];
+		return archive;
 	}
-	archive >> (int&)random.carry;
-	archive >> random.lagPosition;
-
-	return archive;
-}
 
 } // namespace NeoML
+
+// Export C Methods
+#define EXTERN_DLL_EXPORT extern "C" __declspec(dllexport)
+
+EXTERN_DLL_EXPORT void *CRandomInit(unsigned int seed);
+
+EXTERN_DLL_EXPORT void *CRandomInitEmpty();
+
+EXTERN_DLL_EXPORT void CRandomReset(void *ptr, unsigned int seed);
+
+EXTERN_DLL_EXPORT unsigned int CRandomNext(void *ptr);
+
+EXTERN_DLL_EXPORT double CRandomUniform(void *ptr, double min, double max);
+
+EXTERN_DLL_EXPORT int CRandomUniformInt(void *ptr, int min, int max);
+
+EXTERN_DLL_EXPORT double CRandomNormal(void *ptr, double mean, double sigma);
