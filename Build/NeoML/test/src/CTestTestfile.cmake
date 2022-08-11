5f2ade3306e72c6bd1ca61a937324c0571fb7372
Shigomany <ltropin@mail.ru>
2022-08-11 23:48:54 +0500

    Merge

---
+++ Build/NeoML/test/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/shigomany/Documents/workingprojects/dart-neoml/neoml/NeoML/test/src
+# Build directory: /Users/shigomany/Documents/workingprojects/dart-neoml/neoml/Build/NeoML/test/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
